d02d7df15617622d3c88ead681c0dbb53e1f3f00
Hridai Trivedy <51247858+Hridai@users.noreply.github.com>
2023-06-14 12:23:09 +1000

    Update log.py
    fixing duplicate line issue.

--- log.py
+++ log.py
@@ -3,14 +3,15 @@ import logging
 import logging.handlers
 from pathlib import Path
 
-log_dir = os.path.normpath(os.path.expanduser('~/Documents/Claims BI Logs/'))
-Path(log_dir).mkdir(parents=True, exist_ok=True)
 
 def setup(name):
     logger = logging.getLogger(name)
     if logger.hasHandlers():
         return logger
 
+    log_dir = os.path.normpath(os.path.join(os.getcwd(), '..','logs'))
+    Path(log_dir).mkdir(parents=True, exist_ok=True)
+    
     log_fname = os.path.join(log_dir, name + '.log')
     rfh = logging.handlers.RotatingFileHandler(filename = log_fname,
                                                 mode = 'a',
@@ -22,5 +23,5 @@ def setup(name):
     logging.basicConfig(format='%(asctime)s [%(filename)s:%(lineno)d] %(levelname).4s | %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
                         level=logging.INFO,
-                        handlers=[rfh, logging.StreamHandler()])
+                        handlers=[rfh])
     return logger
